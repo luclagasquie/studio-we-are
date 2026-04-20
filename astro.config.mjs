1256f9f26ebad915745d803d829e01e3c0262beb
Luc Lagasquie <sensodia@gmail.com>
2026-04-20 16:45:24 +0200

    fix: change output configuration from 'server' to 'hybrid'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://we-are.fr',
-  output: 'server',
+  output: 'hybrid',
   trailingSlash: 'always',
   adapter: cloudflare({
     platformProxy: {
